caead0da313f8c3ff87255bdd483afd01e69fd74
Amadou Touré <amadouit223@gmail.com>
2023-06-12 10:40:12 +0000

    commentaire sur le polymorphisme de surcharge

--- src/Gestion_pointage/Principale.java
+++ src/Gestion_pointage/Principale.java
@@ -3,15 +3,16 @@ public class Principale {
 
 	public static void main(String[] args) {
 	// Instanciation des objets
-	Apprenant ap1 = new Apprenant(01, "Amadou Touré", "Daoudabougou, près de l'olympe hotel");
-	Formateur fm1 = new Formateur(02, "Momouhoud Dembélé", "Badalabougou, près de la mosquée de L'iman Dicko", 241, "Génie logiciel");
-	Pointage pt1 = new Pointage(54, null, "Présent");
+	Apprenant ap1 = new Apprenant(01, "Amadou Tourï¿½", "Daoudabougou, prï¿½s de l'olympe hotel");
+	Formateur fm1 = new Formateur(02, "Momouhoud Dembï¿½lï¿½", "Badalabougou, prï¿½s de la mosquï¿½e de L'iman Dicko", 241, "Gï¿½nie logiciel");
+	Pointage pt1 = new Pointage(54, null, "Prï¿½sent");
 	Promotion pn1 = new Promotion(54, "ODK3", null);
 	// le polymorphisme d'heritage
      Utilisateur utilisateurApprenant = ap1;
      Utilisateur utilisateurFormateur = fm1;
-     System.out.println("L'apprenant se nommé : "+utilisateurApprenant.getNom()+" et son adresse est : "+ap1.getAdresse());
-     System.out.println("Le formateur est : "+fm1.getNom()+" et sa spécialité est le "+fm1.Spécialité);
+     System.out.println("L'apprenant se nommï¿½ : "+utilisateurApprenant.getNom()+" et son adresse est : "+ap1.getAdresse());
+     System.out.println("Le formateur est : "+fm1.getNom()+" et sa spï¿½cialitï¿½ est le "+fm1.Spï¿½cialitï¿½);
+	 // le polymorphisme de surcharge
 	ap1.methode1("Amadou");
 	fm1.methode1();
 	}
